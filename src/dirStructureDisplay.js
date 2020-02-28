2cfe6dba71cbec0f5f1320d188262649a5a4aaf9
Tovly Deutsch <tovlydeutsch@gmail.com>
2020-02-28 16:47:38 -0500

    deleted debugging printer

--- src/dirStructureDisplay.js
+++ src/dirStructureDisplay.js
@@ -1,23 +1,6 @@
 import React from 'react';
 import NavFile from './components/navFile'
 
-// TODO simplify/delete this function
-function folderStructureToString(file, descendancyLevel) {
-  let totalString = file.name ? file.name + '<br>' : '<br>'
-  if (file.hasOwnProperty('children') && Object.entries(file.children).length !== 0) {
-    for (let fileId in file.children) {
-      let childFile = file.children[fileId]
-      totalString += '&nbsp;'.repeat(descendancyLevel) + childFile.name + '<br>'
-      if (childFile.hasOwnProperty('children')) {
-        for (let file2 of Object.values(childFile.children)) {
-          totalString += ('&nbsp;'.repeat(descendancyLevel) + folderStructureToString(file2, descendancyLevel + 1) + '<br>')
-        }
-      }
-    } 
-  }
-  return totalString
-}
-
 function renderDirStructure(file, level) {
   let children = [];
   if (file.hasOwnProperty('children') && Object.entries(file.children).length > 0) {
@@ -29,4 +12,4 @@ function renderDirStructure(file, level) {
   return <NavFile name={file.name} children={children} key={file.id} level={level}/>
 }
 
-export {folderStructureToString, renderDirStructure}
+export { renderDirStructure }
