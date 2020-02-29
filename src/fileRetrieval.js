7339bbf5da1d68779e5ddd3cc8661eb06a88138d
Tovly Deutsch <tovlydeutsch@gmail.com>
2020-02-29 00:58:02 -0500

    sped up dir building function

--- src/fileRetrieval.js
+++ src/fileRetrieval.js
@@ -10,24 +10,17 @@ async function getFiles() {
     let parameters = {
       'pageSize': 1000,
       'fields': `nextPageToken, files(${fileFields})`,
-      // 'fields': "nextPageToken, files(\
-      // id, name, createdTime, fileExtension, quotaBytesUsed, owners, ownedByMe, webViewLink, mimeType)",
-      // 'q': "mimeType='image/jpeg'",
       q: "trashed = false and 'me' in owners",
-      // spaces: 'drive',
-      // corpora: 'user'
     }
   
     // if not first request, set the pageToken to the next page
     if (nextPageToken) { 
       parameters.pageToken = nextPageToken
     }
-  
-    // adapted from Google Drive API documentation quickstart (https://developers.google.com/drive/v3/web/quickstart/js)
-    numRequests++
     
     try {
       let response = await window.gapi.client.drive.files.list(parameters);
+      numRequests++
       files = files.concat(response.result.files)
       if (response.result.nextPageToken !== nextPageToken) {
         nextPageToken = response.result.nextPageToken
@@ -44,41 +37,55 @@ async function getFiles() {
       // TODO display error to user
       console.log(err)
     }
-  } while (nextPageToken != null && numRequests < 1000);
+  } while (nextPageToken != null && numRequests < 1);
   return files;
 }
 
+// TODO simplify/refactor this function
 async function assembleDirStructure(files) {
   let rootFolderResponse = await window.gapi.client.drive.files.get({fileId: 'root', fields: fileFields})
   let rootFolder = rootFolderResponse.result
-  let rootFolderId = rootFolder.id
   let folderPaths = {}
-  folderPaths[rootFolderId] = []
-  let filedIds = new Set([rootFolderId])
+  folderPaths[rootFolder.id] = []
+  let unfiledFiles = new Set(files)
   rootFolder.children = {}
 
-  function placeFiles() {
-    for (let file of files) {
-      if (file.parents && folderPaths.hasOwnProperty(file.parents[0]) && !filedIds.has(file.id)) {
-        let path = folderPaths[file.parents[0]]
-        let currentObj = rootFolder
-        for (let el of path) {
-          currentObj = currentObj.children[el]
-        }
-        if (!currentObj.hasOwnProperty('children')) { currentObj.children = {}}
-        currentObj.children[file.id] = file
-        if (file.mimeType === 'application/vnd.google-apps.folder') {
-          folderPaths[file.id] = path.concat(file.id)
-        }
-      }
+  for (let file of unfiledFiles) {
+    let path = [];
+    let dummyParent = true
+    let parentId = file.parents[0]
+    if (folderPaths.hasOwnProperty(parentId)) {
+      path = folderPaths[parentId]
+      dummyParent = false;
+    }
+
+    if (dummyParent) {  // if a path to parent doesn't exist, we need a dummy node for parent
+      rootFolder.children[parentId] = {dummy: true, id: parentId, children: {}}
+      path = [parentId]
+    }
+
+    let currentObj = rootFolder
+    for (let el of path) {
+      currentObj = currentObj.children[el]
+    }
+    if (!currentObj.hasOwnProperty('children')) { currentObj.children = {}}
+    currentObj.children[file.id] = file
+    if (rootFolder.hasOwnProperty(file.id) && rootFolder[file.id].dummy) {
+      // TODO see if this object copy is not necessary
+      file.children = JSON.parse(JSON.stringify(rootFolder[file.id].children))
+      delete rootFolder[file.id]
+    }
+    if (file.mimeType === 'application/vnd.google-apps.folder') {
+      folderPaths[file.id] = path.concat(file.id)
     }
   }
-
-  let numAttempts = 0
-  while (filedIds.size < files.length && numAttempts < 20) {
-    placeFiles()
-    numAttempts++
-  } 
+  
+  // prune dummy nodes
+  for (let rootNestedFileId in rootFolder.children) {
+    if (rootFolder.children[rootNestedFileId].dummy) {
+      delete rootFolder.children[rootNestedFileId]
+    }
+  }
   // TODO consider reformatting here to avoid the constant need for Object.Values
   annotateFileSizes(rootFolder)
   return rootFolder
@@ -91,6 +98,7 @@ async function getAssembledDirStruct() {
     files = JSON.parse(filesStored)
   }
   else {
+    console.log('fetching files')
     files = await getFiles();
   }
   return await assembleDirStructure(files)
